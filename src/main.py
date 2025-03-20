614e2f70ffaa76582287e98bb2c2b7d348447ab8
Nawaz Khurshid <nawaz1981@gmail.com>
2025-03-20 16:50:12 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
         download_from_object_json(download_parameters)
     # currentpath_files = DOWNLOAD_PATH
     
-    if (download_parameters.log_preprocess_only):
+    if (download_parameters.log_preprocess_only()):
         currentpath_files = preprocess_path
         print(f"Logging only preprocess data from {currentpath_files}")
     else:
